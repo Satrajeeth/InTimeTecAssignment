48e422df8a31853125183663036bbb7975964ac8
S Satrajeeth Reddy <147574438+Satrajeeth@users.noreply.github.com>
2025-11-06 09:06:59 +0530

    Implement cycle detection in a linked list

---
+++ CycleDectectionfunction.c
@@ -0,0 +1,17 @@
+bool has_cycle(SinglyLinkedListNode* head) {
+    if (head == NULL) return false;  
+
+    SinglyLinkedListNode* slow = head;
+    SinglyLinkedListNode* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;           
+        fast = fast->next->next;     
+
+        if (slow == fast) {
+            return true;             
+        }
+    }
+
+    return false;                    
+}
